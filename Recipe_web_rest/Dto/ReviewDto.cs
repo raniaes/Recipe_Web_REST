d31fed99364a54bdf3937df95af8ca4d9e6737a8
Geunmok Song <thdrmsahr123@gmail.com>
2024-08-02 13:46:50 +1200

    modify review

--- Recipe_web_rest/Dto/ReviewDto.cs
+++ Recipe_web_rest/Dto/ReviewDto.cs
@@ -3,7 +3,7 @@
     public class ReviewDto
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public DateTime Date { get; set; }
         public string Content { get; set; }
         public int Rating { get; set; }
         public int UserId { get; set; }
--- Recipe_web_rest/Migrations/20240802011157_InitialCreate.cs
+++ Recipe_web_rest/Migrations/20240802011157_InitialCreate.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -107,7 +108,7 @@ namespace Recipe_web_rest.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
                     Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Rating = table.Column<int>(type: "int", nullable: false),
                     UserId = table.Column<int>(type: "int", nullable: false),
